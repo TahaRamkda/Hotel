9a893e8477e097ec3ff3b86abf00461998b19261
TahaRamkda <taharamkda979@gmail.com>
2024-10-30 20:17:39 +0530

    test updates

--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -2,36 +2,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HotelTest {
-
-    @Test
-public void testRoomAllocation() {
-    Hotel.hotel_ob = new holder(); // Reset holder object for testing
-
-    // Call the method to input customer details and allocate a room
-    Hotel.CustDetails(1, 0); // Allocating a luxury double room (case 1)
-
-    System.out.println("Room allocated: " + Hotel.hotel_ob.luxury_doublerrom[0]); // Debug line
-    // Verify that the room has been allocated
-    assertNotNull(Hotel.hotel_ob.luxury_doublerrom[0], "Room should be allocated");
-}
-
-@Test
-public void testRoomDeallocate() {
-    Hotel.hotel_ob = new holder();
-    
-    // First, allocate a room
-    Hotel.CustDetails(1, 0); 
-    assertNotNull(Hotel.hotel_ob.luxury_doublerrom[0], "Room should be allocated");
-
-    // Then deallocate the room
-    Hotel.deallocate(0, 1); // Deallocate the room
-
-    System.out.println("Room after deallocation: " + Hotel.hotel_ob.luxury_doublerrom[0]); // Debug line
-    // Verify that the room is deallocated
-    assertNull(Hotel.hotel_ob.luxury_doublerrom[0], "Room should be deallocated");
-}
-
-
     @Test
     public void testInvalidRoomNumber() {
         // Trying to deallocate a room that doesn't exist
